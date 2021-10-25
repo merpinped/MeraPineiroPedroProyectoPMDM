06ce85aa51c828bf2f2937904db7690d9427b354
merpinped <tics.dima.mera.p@gmail.com>
2021-10-25 09:08:47 +0200

    cambios 1

--- app/src/main/java/es/murallaromana/proyecto/LoginActivity.kt
+++ app/src/main/java/es/murallaromana/proyecto/LoginActivity.kt
@@ -1,13 +1,20 @@
 package es.murallaromana.proyecto
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var btnRegistrar: Button
+    private lateinit var usuario: RegistraActivity
+    private lateinit var tiEmail: TextInputEditText
+    private lateinit var tiPassword: TextInputEditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +23,14 @@ class LoginActivity : AppCompatActivity() {
         btnRegistrar = findViewById(R.id.btnRegistro)
 
         btnRegistrar.setOnClickListener() {
-            var intent = Intent(this, RegistraActivity::class.java)
+            val intent = Intent(this, RegistraActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        tiEmail = findViewById(R.id.tiEmail)
+        tiPassword = findViewById(R.id.tiPassword)
     }
 }
--- app/src/main/java/es/murallaromana/proyecto/RegistraActivity.kt
+++ app/src/main/java/es/murallaromana/proyecto/RegistraActivity.kt
@@ -1,11 +1,86 @@
 package es.murallaromana.proyecto
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
+
+import android.content.SharedPreferences
+import android.util.Log
+
 
 class RegistraActivity : AppCompatActivity() {
+
+    private lateinit var btnNuevoUsuario: Button
+    private lateinit var tiEmail: TextInputEditText
+    private lateinit var tiPassword: TextInputEditText
+    private lateinit var tiConfirmar: TextInputEditText
+    private lateinit var tiNombre: TextInputEditText
+    private lateinit var tiTelefono: TextInputEditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registra)
+
+        btnNuevoUsuario = findViewById(R.id.btnRegistro)
+        tiEmail = findViewById(R.id.tiEmail)
+        tiPassword = findViewById(R.id.tiPassword)
+        tiConfirmar = findViewById(R.id.tiConfirmar)
+        tiNombre = findViewById(R.id.tiNombre)
+        tiTelefono = findViewById(R.id.tiTelefono)
+
+        btnNuevoUsuario.setOnClickListener {
+
+            // Comprueba si todos los campos estan completos
+            if (TextUtils.equals(
+                    tiEmail.text.toString(),
+                    ""
+                ) && TextUtils.equals(tiPassword.text.toString(), "")
+                && TextUtils.equals(
+                    tiConfirmar.text.toString(),
+                    ""
+                ) && TextUtils.equals(
+                    tiNombre.text.toString(),
+                    ""
+                )
+                && TextUtils.equals(tiTelefono.text.toString(), "")
+            ) {
+
+                Log.d("RegistrarActivity", "Campos vacíos")
+
+            } else {
+                if (TextUtils.equals(
+                        tiPassword.text.toString(),
+                        tiConfirmar.text.toString()
+                    )
+                ) { // Comprueba si confirmar y password son iguales
+
+                    val sharedPref =
+                        getPreferences(Context.MODE_PRIVATE) // Guardar objetos clave valor
+                    val editor = sharedPref.edit() // Iniciamos una transacción
+
+                    editor.putString("email", tiEmail.text.toString())
+                    editor.putString("password", tiPassword.text.toString())
+
+                    editor.apply()
+
+                    onBackPressed() // Vuelve a la pantalla anterior
+                } else {
+                    Log.d("RegistrarActivity", "La contraseña y confirmar son distintas")
+                }
+            }
+        }
+
+        fun getEmail(): String? {
+            val preferences = getPreferences(Context.MODE_PRIVATE)
+            return preferences.getString("email", "")
+        }
+
+        fun getPassword(): String? {
+            val preferences = getPreferences(Context.MODE_PRIVATE)
+            return preferences.getString("password", "")
+        }
     }
 }
